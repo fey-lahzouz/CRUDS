3a9a71c6a23c29ed67f93df40fa879e669ca6ef6
Fayza <fayza.lahzouz@gmail.com>
2022-11-22 00:44:29 +0100

    Routing

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ const express = require('express')
 const moongose = require('moongose')
 const url = 'mongodb://localhost/Book'
 
-const app = express()
+const app = express() // start express framework
 
+// connect to db
 moongose.connect(url)
 const con = moongose.connetion
 
-con.on('open', function(){
+con.on('open',  () => {
 console.log('connection')
 })
 
-app.listen(9000, function(){
+const bookRouter = require('../routes/books.js')
+app.use('/books', bookRouter) // /middleware -> to all the books request send them to the bookRouter
+app.listen(9000, () => {
     console.log('Server Running')
 })
--- routes/books.js
+++ routes/books.js
@@ -1,2 +1,15 @@
 const express = require('express')
 const router = express.Router()
+
+router.get('/', async(req, res) => { // async -> not block the process
+    //console.log('Get started')
+    res.send('Get started')
+
+    try {
+        
+    } catch (error) {
+        res.send('Error ' + error)
+    }
+})
+
+module.exports = router // export router module so it can be accessible in the app js
